2da773c83afe735f7512c41465b9556b1ed2af83
Nagarajb95 <57646286+Nagarajb95@users.noreply.github.com>
2023-06-01 07:31:48 +0530

    Update Automation.sh

--- Automation.sh
+++ Automation.sh
@@ -1,1 +1,14 @@
-
+sudo apt update
+sudo apt install apache2
+sudo ufw app list
+sudo ufw allow 'Apache'
+sudo ufw status
+sudo systemctl status apache2
+hostname -I
+curl -4 icanhazip.com
+sudo systemctl stop apache2
+sudo systemctl start apache2
+sudo systemctl restart apache2
+sudo systemctl reload apache2
+sudo systemctl disable apache2
+sudo systemctl enable apache2
